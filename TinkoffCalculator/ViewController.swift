fef0048c2f72318759945d9ed7f380606107b8a5
Drollllted <artemdrolllted@gmail.com>
2024-02-08 18:35:02 +0300

    Initial Commit

---
+++ TinkoffCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TinkoffCalculator
+//
+//  Created by Drolllted on 08.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

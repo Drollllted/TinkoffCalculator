16f8847c1b38b9a6ff30f1d906591209ed070971
Drollllted <artemdrolllted@gmail.com>
2024-02-08 18:53:01 +0300

    Function

--- TinkoffCalculator/ViewController.swift
+++ TinkoffCalculator/ViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        guard let buttonPressed = sender.titleLabel?.text else {return}
+        
+        print(buttonPressed)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
